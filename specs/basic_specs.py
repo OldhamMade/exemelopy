f4365613a2a2b3a93a036978d54974afb2cc4aa4
Phillip B Oldham <phillip.oldham@gmail.com>
2011-05-03 21:28:15 +0100

    initial commit of working object and specs

---
+++ specs/basic_specs.py
@@ -0,0 +1,272 @@
+from collections import MutableMapping as DictMixin
+import datetime
+import os
+import re
+import sys
+import unittest
+
+BASE_PATH = '/'.join(os.path.dirname(os.path.abspath(__file__)).split('/')[0:-1])
+
+if BASE_PATH not in sys.path:
+    sys.path.insert(1, BASE_PATH)
+
+from exemelopy import XMLEncoder
+
+class PlainObject(object):
+    pass
+
+class ComplexObject(DictMixin):
+    def __init__(self): self.dict = dict()
+    def __delitem__(self, key): del self.dict[key]
+    def __getitem__(self, key): return self.get(key, KeyError)
+    def __iter__(self): return iter(self.dict)
+    def __len__(self): return len(self.dict)
+    def __setitem__(self, key, value): self.append(key, value)
+
+    def get(self, key, default=None):
+        if key not in self.dict:
+            return default
+        return self.dict[key]
+
+
+
+class CommonBaseSpec(unittest.TestCase):
+    def _format_each_should_equal(self, items):
+        for test, expected in items:
+            output = XMLEncoder(test).to_string()
+            self.assertEqual(output, expected)
+
+
+
+class BasicSpec(CommonBaseSpec):
+
+    def it_should_format_simple_objects(self):
+        tests = (
+            (None, '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document/>'),
+            ('test', '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document>test</document>'),
+            )
+
+        self._format_each_should_equal(tests)
+
+
+    def it_should_format_lists(self):
+        tests = (
+            ([1,2,3], '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document nodetype="list">\n  <i>1</i>\n  <i>2</i>\n  <i>3</i>\n</document>\n'),
+            )
+
+        self._format_each_should_equal(tests)
+    
+
+    def it_should_format_sets(self):
+        tests = (
+            (set([1,2,3]), '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document nodetype="unique-list">\n  <i>1</i>\n  <i>2</i>\n  <i>3</i>\n</document>\n'),
+            )
+
+        self._format_each_should_equal(tests)
+    
+
+    def it_should_format_tuples(self):
+        tests = (
+            ((1,2,3), '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document nodetype="fixed-list">\n  <i>1</i>\n  <i>2</i>\n  <i>3</i>\n</document>\n'),
+            )
+
+        self._format_each_should_equal(tests)
+    
+
+    def it_should_format_generator_objects(self):
+        tests = (
+            ((i for i in xrange(1,4)), '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document nodetype="generated-list">\n  <i>1</i>\n  <i>2</i>\n  <i>3</i>\n</document>\n'),
+            )
+
+        self._format_each_should_equal(tests)
+    
+
+    def it_should_format_nested_unicode_dicts(self):
+        tests = (
+            ({u'foo': u'bar'},
+             '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document>\n  <foo>bar</foo>\n</document>\n'),
+            ({u'foo': {u'bar': u'baz'}}, '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document>\n  <foo>\n    <bar>baz</bar>\n  </foo>\n</document>\n'),
+            ({u'packages': [],
+              u'terms': None,
+              u'description': {
+                  u'short': u'sldf',
+                  u'long': u'skdjhfskjfdgn'
+                  },
+              u'title': u'meh',
+              u'expires': datetime.datetime(2010, 2, 1, 0, 0),
+              u'venues': [],
+              u'activity': None,
+              u'_id': '4d2596f7fa5bd80e18000001',
+              u'type': None
+              },
+             """<?xml version='1.0' encoding='UTF-8'?>
+<document>
+  <activity/>
+  <expires nodetype="timestamp">2010-02-01T00:00:00</expires>
+  <terms/>
+  <description>
+    <short>sldf</short>
+    <long>skdjhfskjfdgn</long>
+  </description>
+  <title>meh</title>
+  <packages nodetype="list"/>
+  <type/>
+  <venues nodetype="list"/>
+  <node name="_id">4d2596f7fa5bd80e18000001</node>
+</document>
+"""),
+            )
+
+        self._format_each_should_equal(tests)
+
+
+    def it_should_format_special_characters(self):
+        tests = (
+            ('< & >', '<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document>&lt; &amp; &gt;</document>'),
+            )
+
+        self._format_each_should_equal(tests)
+
+
+    def it_should_format_newlines_correctly(self):
+        tests = (
+            ('''This
+
+is
+
+a
+
+long
+
+paragraph
+
+''', '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document>This
+
+is
+
+a
+
+long
+
+paragraph
+
+</document>'''),
+            (('''This
+
+is
+
+a''', '''
+
+long
+
+paragraph
+
+'''), '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>\n<document nodetype="fixed-list">
+  <i>This
+
+is
+
+a</i>
+  <i>
+
+long
+
+paragraph
+
+</i>
+</document>
+'''),
+            )
+
+        self._format_each_should_equal(tests)
+
+
+    def it_should_format_complex_objects(self):
+
+        nesteddict = {
+            'foo': set([1,2,3]),
+            'bar': False,
+            'baz': {
+                'a': (1,2,3),
+                'b': datetime.date(2002, 3, 11),
+                'c': "{36980915-cd66-4547-9081-760ad0d77625}"
+                }
+            }
+
+        foo = PlainObject()
+        foo.bar = 'baz'
+        foo.nested = nesteddict
+        
+        tests = (
+            ((1,2,3), '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>
+<document nodetype="fixed-list">
+  <i>1</i>
+  <i>2</i>
+  <i>3</i>
+</document>
+'''),
+            (['foo','bar',True],
+             '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>
+<document nodetype="list">
+  <i>foo</i>
+  <i>bar</i>
+  <i nodetype="boolean">true</i>
+</document>
+'''),
+            ((i for i in [1,2,3]),
+             '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>
+<document nodetype="generated-list">
+  <i>1</i>
+  <i>2</i>
+  <i>3</i>
+</document>
+'''),
+            (nesteddict,
+             '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>
+<document>
+  <baz>
+    <a nodetype="fixed-list">
+      <i>1</i>
+      <i>2</i>
+      <i>3</i>
+    </a>
+    <c nodetype="uuid">{36980915-cd66-4547-9081-760ad0d77625}</c>
+    <b nodetype="timestamp">2002-03-11</b>
+  </baz>
+  <foo nodetype="unique-list">
+    <i>1</i>
+    <i>2</i>
+    <i>3</i>
+  </foo>
+  <bar nodetype="boolean">false</bar>
+</document>
+'''),
+            (foo,
+             '''<?xml version=\'1.0\' encoding=\'UTF-8\'?>
+<document>
+  <PlainObject nodetype="container">
+    <bar>baz</bar>
+    <nested>
+      <baz>
+        <a nodetype="fixed-list">
+          <i>1</i>
+          <i>2</i>
+          <i>3</i>
+        </a>
+        <c nodetype="uuid">{36980915-cd66-4547-9081-760ad0d77625}</c>
+        <b nodetype="timestamp">2002-03-11</b>
+      </baz>
+      <foo nodetype="unique-list">
+        <i>1</i>
+        <i>2</i>
+        <i>3</i>
+      </foo>
+      <bar nodetype="boolean">false</bar>
+    </nested>
+  </PlainObject>
+</document>
+'''),
+            )
+
+        self._format_each_should_equal(tests)
+
