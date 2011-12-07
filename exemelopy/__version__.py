02ddc7891c387d4d0b80617e5b8a2b77e9bba6cb
Phillip Oldham <phillip.oldham@gmail.com>
2011-12-07 21:42:01 +0000

    updated for distribution

--- exemelopy/__version__.py
+++ exemelopy/__version__.py
@@ -1,3 +1,4 @@
 __author__	= 'Phillip B Oldham'
+__author_email__ = 'phillip.oldham@gmail.com'
 __version__	= '0.0.1'
 __licence__	= 'MIT'
--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import os, sys
 execfile('exemelopy/__version__.py')
 
 setup(name="exemelopy",
-      description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+      description="exemelopy is a tool for building XML from native Python data-types, similiar to the json/simplejson modules",
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
       version=__version__,
-      url="http://unpluggd/exemelopy",
+      url="http://unpluggd.github.com/exemelopy",
+      author=__author__,
+      author_email=__author_email__,
       packages=find_packages(exclude=["specs","benchmark"]),
       install_requires=['lxml','ordereddict'],
       )
-      
